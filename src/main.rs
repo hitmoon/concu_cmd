dc19006446c1a715091923f15970b04034e47a02
xiaoqiang zhao <zxq_yx_007@163.com>
2022-04-22 22:28:29 +0800

    print execute output to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,60 @@
 use clap::{arg, Command};
-use std::path::Path;
-use std::io::Read;
+use std::io::{self, Read, Write};
 use std::fs::File;
 use rayon::prelude::*;
+use std::process;
 
 fn main() {
 
     let mut argf = String::new();
+    let mut binf = String::new();
+    let mut earg = String::new();
 
     let cmd = Command::new("conm")
         .version("0.1")
         .author("zxq_yx_007@163.com")
-        .arg(arg!(-f --file [arg_file]  "file to read args from").required(true));
+        .arg(arg!(-b --bin  <bin_file>  "binary file to execute").required(true))
+        .arg(arg!(-a --args [extra_args]   "extra args passwd to").required(false))
+        .arg(arg!(-f --file <arg_file>  "file to read args from").required(true));
 
     let matches = cmd.get_matches();
 
     if let Some(f) = matches.value_of("file") {
-       argf = f.to_string();
+        argf = f.to_string();
+    }
+
+    if let Some(b) = matches.value_of("bin") {
+        binf = b.to_string();
+    }
+
+    if let Some(e) = matches.value_of("args") {
+        earg = e.to_string();
     }
 
     let mut f = File::open(argf).unwrap();
 
     let mut cont = String::new();
-    f.read_to_string(&mut cont);
+    f.read_to_string(&mut cont).unwrap();
 
-    let lines: Vec<String> = cont.split('\n').collect();
-    lines().par_iter()
-    .map(|arg| { println!("{}", arg) })
+    let lines:Vec<String> = cont.split('\n').map(String::from).collect();
+    lines.par_iter()
+    .for_each(|arg| {
+        let m = arg.trim();
+        if m != "" {
+            let cmd = format!("{} {} {}", &binf, m, &earg);
+            println!("Executing: {}", cmd);
+            let mut cmds = vec![m];
+            if earg != "" {
+                cmds.push(&earg[..]);
+            }
     
+            let output = process::Command::new(&binf)
+                                .args(cmds)
+                                .output()
+                                .expect("failed to execute!");
+            println!("status: {}", output.status);
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+        }
+    });
 }
